365c42bfb548a04c55e53509ad2fe11e9ac15729
Kirakle <83728623+Kirakle@users.noreply.github.com>
2022-05-24 20:16:11 +0300

    fix: change update files for remove

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,14 +1,17 @@
-const fs = require('fs');
 const path = require('path');
+const fs = require('fs');
 
-fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, err => {
-    if (err) throw err;
-});
-
-fs.readdir(path.join(__dirname, 'files'), (err, files) => {
-    if (err) throw err;
-    files.forEach(file => {
-        fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), (err) => { if (err) throw err });
-    })
+fs.rm(path.join(__dirname, 'files-copy'), { recursive: true }, () => {
+    fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, () => { });
+    fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+        if (err) throw err;
+        files.forEach((file) => {
+            fs.copyFile(
+                path.join(__dirname, 'files', file),
+                path.join(__dirname, 'files-copy', file),
+                () => { }
+            );
+        });
+    });
 });
 
